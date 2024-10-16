fe03dbba92ebbcb5d4c27f4e1e176e444f2a8cf8
전상은 <wowddok99@gmail.com>
2024-10-17 01:00:08 +0900

    first commit

---
+++ src/main/java/com/example/codeconvoproject/CodeconvoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeconvoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeconvoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeconvoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/codeconvoproject/CodeconvoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeconvoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeconvoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
