cc38bf89dd16c11caea204c0c14953fff99740e7
전상은 <wowddok99@gmail.com>
2024-11-23 14:11:22 +0900

    feat: fetchPosts API 최신순으로 정렬되도록 수정

--- src/main/java/com/example/codeconvoproject/service/PostService.java
+++ src/main/java/com/example/codeconvoproject/service/PostService.java
@@ -11,6 +11,7 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -139,8 +140,11 @@ public class PostService {
     }
 
     public FetchPostsResponse fetchPosts(Long categoryId, int pageNumber, int size){
+        // Sort 객체를 생성하여 정렬 기준을 설정합니다.
+        Sort sort = Sort.by(Sort.Direction.DESC, "createdAt");
+
         // 페이지 번호와 페이지 크기를 사용하여 PageRequest 객체를 생성합니다.
-        PageRequest pageRequest = PageRequest.of(pageNumber, size);
+        PageRequest pageRequest = PageRequest.of(pageNumber, size, sort);
 
         Page<Post> fetchedPosts = postRepository.findByCategoryId(categoryId, pageRequest);
 
